7255709d928e497d3d17562d4d4aa7504cb96371
Riddhi Patil <riddhipatil1905@gmail.com>
2025-07-27 19:23:06 +0530

    initial commit

---
+++ runn-tracker-backend/src/main/java/com/riddhi/runntrackerbackend/RunnTrackerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.riddhi.runntrackerbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RunnTrackerBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RunnTrackerBackendApplication.class, args);
+    }
+
+}
