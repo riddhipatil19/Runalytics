82e31e919a86d87e95544ec84432659e7ec8f2f7
Riddhi Patil <riddhipatil1905@gmail.com>
2025-07-27 21:30:18 +0530

    add user entity

---
+++ runn-tracker-backend/src/main/java/com/riddhi/runntrackerbackend/entity/User.java
@@ -0,0 +1,34 @@
+package com.riddhi.runntrackerbackend.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 100)
+    private String name;
+
+    @Column(nullable = false, unique = true, length = 100)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Run> runs;
+}
